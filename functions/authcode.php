c867cd7acdaac33102db385554ffcb13badc7d40
emekach <chukwuemeka.chukwuma11@gmail.com>
2024-03-01 19:49:41 +0000

    authentication page and validation

---
+++ functions/authcode.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+
+include('../config/dbcon.php');
+
+if (isset($_POST['register_btn'])) {
+
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+
+
+    $email_check_query = "SELECT email FROM users WHERE email= '$email'";
+    $email_check_query_run = mysqli_query($con, $email_check_query) or die(mysqli_error($con));
+
+    if (mysqli_num_rows($email_check_query_run) > 0) {
+
+        $_SESSION['email_check_message'] = "Email Already exists in database";
+        header('Location: ../register.php');
+        die();
+    } else {
+        if ($password == $cpassword) {
+
+            $insert_query = "INSERT INTO users (name,phone,email,password) VALUES ('$name','$phone','$email','$password')";
+            $insert_query_run = mysqli_query($con, $insert_query) or die(mysqli_error($con));
+
+            if ($insert_query_run) {
+
+                $_SESSION['message'] = "Registered Successfully";
+                header('Location: ../login.php');
+                die();
+            } else {
+
+                $_SESSION['message'] = "Something went wrong";
+                header('Location: ../register.php');
+                die();
+            }
+        } else {
+
+            $_SESSION['password_mismatch'] = "Passwords do not match";
+            header('Location: ../register.php');
+            die();
+        }
+    }
+}
+
+
+if (isset($_POST['login_btn'])) {
+
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $login_query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $login_query_run = mysqli_query($con, $login_query) or die(mysqli_error($con));
+
+    if (mysqli_num_rows($login_query_run) > 0) {
+
+        $_SESSION['auth'] = true;
+
+        $userdata = mysqli_fetch_array($login_query_run);
+        $username = $userdata['name'];
+        $useremail = $userdata['email'];
+
+        // print_r($userdata);
+
+        $_SESSION['auth_user'] = [
+            'name' => $username,
+            'email' => $useremail,
+        ];
+
+        $_SESSION['message'] = "Logged In Successfully";
+
+        header('Location: ../index.php');
+        die();
+    } else {
+
+        $_SESSION['message'] = "Invalid credentials";
+
+        header('Location: ../login.php');
+
+        die();
+    }
+}
---
+++ register.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+include('includes/header.php');
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+
+                <?php
+
+                if (isset($_SESSION['message'])) {
+                ?>
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>Hey!</strong> <?= $_SESSION['message']; ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+
+                <?php
+                    unset($_SESSION['message']);
+                }
+
+                ?>
+                <div class="card">
+                    <div class="card-header">
+
+                        <h4> Registeration Form</h4>
+
+                    </div>
+                    <div class="card-body">
+                        <form action="functions/authcode.php" method="POST">
+
+                            <div class="mb-3">
+                                <label class="form-label">Name</label>
+                                <input type="text" name="name" class="form-control" placeholder="Enter Your Name">
+                                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">Phone</label>
+                                <input type="number" name="phone" class="form-control" placeholder="Enter Your Phone Number">
+
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">Email address</label>
+                                <input type="email" name="email" class="form-control" placeholder="Enter Your Email">
+
+                            </div>
+                            <?php
+
+                            if (isset($_SESSION['email_check_message'])) {
+                            ?>
+                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                    <strong>Hey!</strong> <?= $_SESSION['email_check_message']; ?>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+
+                            <?php
+                                unset($_SESSION['email_check_message']);
+                            }
+
+                            ?>
+                            <div class="mb-3">
+                                <label class="form-label">Password</label>
+                                <input type="password" name="password" class="form-control" placeholder="Enter Your password">
+                            </div>
+
+                            <?php
+
+                            if (isset($_SESSION['password_mismatch'])) {
+                            ?>
+                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                    <strong>Hey!</strong> <?= $_SESSION['password_mismatch']; ?>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+
+                            <?php
+                                unset($_SESSION['password_mismatch']);
+                            }
+
+                            ?>
+
+                            <div class="mb-3">
+                                <label class="form-label">Confirm Password</label>
+                                <input type="password" name="cpassword" class="form-control" placeholder="Enter Your Password Again">
+                            </div>
+
+
+
+                            <button type="submit" name="register_btn" class="btn btn-primary">Create Account</button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('includes/footer.php'); ?>
